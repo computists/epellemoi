8a36659ca07cb4d24b6e63a1cd449dd9451566cb
Hong <computists@gmail.com>
2023-01-10 03:20:44 -0500

    chage BrowserRouter to HashRouter

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+//import { BrowserRouter } from 'react-router-dom';
+import { HashRouter as Router } from "react-router-dom";
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
 //import rootReducer from './reducers/rootReducer';
@@ -15,11 +16,11 @@ const store = configureStore(postsSlice);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   //<React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <Provider store={store}>
         <App />
       </Provider>
-    </BrowserRouter>
+    </Router>
   //</React.StrictMode>
 );
 
